ad9c30b309931c1571009894c78a12ef2618e565
Dominik Smagacz <dominik.smagacz@gmail.com>
2023-04-18 22:55:22 +0200

    small updates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -55,9 +55,6 @@
                                     <div class="flex-shrink-0">
                                         <a href="#">
                                             <span class="sr-only">{{ $post->author_id }}</span>
-                                            <img class="h-10 w-10 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt="">
                                         </a>
                                     </div>
                                     <div class="ml-3">
